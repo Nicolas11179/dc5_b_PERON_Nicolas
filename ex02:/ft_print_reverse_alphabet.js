481f6c2b847631a7ca190842def4f58d2a7b5dde
Nicolas11179 <nicoperon64@gmail.com>
2022-11-29 15:52:05 +0100

    Mise en fonction exercice 2

--- ex02:/ft_print_reverse_alphabet.js
+++ ex02:/ft_print_reverse_alphabet.js
@@ -1,3 +1,5 @@
-let alphabet = ['b', 'a', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+function alphabet() {
+    let lettre = ['b', 'a', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-console.log(alphabet.sort().reverse().join());
+    return(lettre.sort().reverse().join());
+}
