4fe03e42dd8b89a20e86a3dde605de3dbf884e1e
Nicolas11179 <nicoperon64@gmail.com>
2022-11-29 15:53:57 +0100

    Mise en fonction exercice 03

--- ex03:/ft_print_numbers.js
+++ ex03:/ft_print_numbers.js
@@ -1,3 +1,7 @@
-let numbers = ['2', '1', '3', '4', '5', '7', '8', '6', '9'];
+function nombre() {
 
-console.log(numbers.sort().join());
+    let numbers = ['2', '1', '3', '4', '5', '7', '8', '6', '9'];
+
+    return(numbers.sort().join());
+
+}
