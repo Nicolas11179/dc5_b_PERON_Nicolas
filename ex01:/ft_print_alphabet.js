1c5c36d97aa8ff07d5c21bc715c1e9210adf203c
Nicolas11179 <nicoperon64@gmail.com>
2022-11-29 15:49:31 +0100

    Mise en fonction exercice 01

--- ex01:/ft_print_alphabet.js
+++ ex01:/ft_print_alphabet.js
@@ -1,3 +1,7 @@
-let alphabet = ['b', 'a', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+function alphabet() {
+    let lettre = ['b', 'a', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-console.log(alphabet.sort().join());
+    return(lettre.sort().join());
+}
+
+
